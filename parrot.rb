c830ccc4219d87f6b71ba33dff20921c20bef327
savannahluy <savannahqluy@gmail.com>
2016-07-26 21:36:27 +0000

    done.

--- parrot.rb
+++ parrot.rb
@@ -4,25 +4,38 @@ def happy_parrot(thing)
 end
 
 # Boring Parrot - Write a method for a boring parrot that just returns whatever string you give him as an argument.
+def boring_parrot(repeat)
+  "I am a boring parrot, i will copy whatever you say, #{repeat}"
+end
 
-
-
-# Math Parrot - Create a method that accepts two numbers as arguments and adds them together! 
-
-
+# Math Parrot - Create a method that accepts two numbers as arguments and adds them together!
+def smart_parrot(x, y)
+  sum = x.to_i + y.to_i
+  "What is #{x} + #{y}? The answer is #{sum}!"
+end
 
 # Friendly Parrot - This parrot greets people by returning their name and age (don't forget to pass that information in as arguments).
-
-
+ def friendly_parrot(name, age)
+   "Hello #{name}! You are #{age} years old!"
+ end
 
 # Favorites Parrot - Tell this parrot about your three favorite things and he will return "I love <that thing> too!" for each of them.
-
-
+def favorites_parrot(fav_1, fav_2, fav_3)
+   "I love #{fav_3} too!"
+end
 
 # Now try calling your methods below with any arguments of your choice and puts them to the screen. Like this:
-puts happy_parrot("waffles")
-# call your methods here
 
+# call your methods here
+puts happy_parrot("waffles")
+
+puts friendly_parrot("Savannah", "14")
+
+puts boring_parrot("truck")
+
+puts smart_parrot("2", "6")
+
+puts favorites_parrot("ice_cream", "cupcakes", "cookies")
 
 # Now let's pretend you are a wizard and you want to place a spell on each of the parrots so that they speak backwards. How would you do that?
 
